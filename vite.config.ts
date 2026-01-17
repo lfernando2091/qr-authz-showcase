66b08d749e607961fd259a26d318a05c5535e0dd
lfernando <lfernando2091@gmail.com>
2026-01-17 15:27:51 -0600

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['459f249e8330.ngrok-free.app']
-  }
+  plugins: [react()]
 })
