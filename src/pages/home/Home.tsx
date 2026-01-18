8abd26df6db7573c7dd39b1a0b7eea6256fb6871
lfernando <lfernando2091@gmail.com>
2026-01-18 15:53:55 -0600

    home updated

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,11 +6,17 @@ export const HomePage = () => {
         <NavLink to="/checkout/abc" end>
             Checkout
         </NavLink>
+        <br/>
         <NavLink to="/scan-qr" end>
             Scan QR
         </NavLink>
+        <br/>
         <NavLink to="/store" end>
             Store
+        </NavLink>
+        <br/>
+        <NavLink to="/shopping-cart" end>
+            Shopping Cart
         </NavLink>
     </>)
 }
